f9a5047008d0786b0630f8838b9d9c95018c2a94
zh <702021619@qq.com>
2021-03-29 08:55:31 +0800

    ..

--- src/main/java/com/zhuhao/demomybatis/DemoMybatisApplication.java
+++ src/main/java/com/zhuhao/demomybatis/DemoMybatisApplication.java
@@ -4,10 +4,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @MapperScan("com.zhuhao.demomybatis.blog.mapper")
-
+@EnableSwagger2
 public class DemoMybatisApplication {
 
     public static void main(String[] args) {
